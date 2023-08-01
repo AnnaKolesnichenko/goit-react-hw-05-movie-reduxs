53d669c0184fc5db763cf93b53aff5550aacc5a6
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-08-01 17:47:39 +0300

    fix

--- src/redux/movieDetailsReducer.js
+++ src/redux/movieDetailsReducer.js
@@ -4,50 +4,51 @@ const initialState = {
     error: false,
   };
   
-  export const postDetailReducer = (state = initialState, action) => {
-    switch(action.type) {
-      case "movie/setIsLoading": {
-        return {
-          ...state,
-          isLoading: action.payload
-        }
-      }
-      case "movie/setMovieDetails": {
-        return {
-          ...state,
-          movie: action.payload
-        }
-      }
-      case "movie/setError": {
-        return {
-          ...state,
-          error: action.payload
-        }
-      }
-      default: return state;
-    }
-  };
+  // REDUX approach 
+  //export const postDetailReducer = (state = initialState, action) => {
+  //   switch(action.type) {
+  //     case "movie/setIsLoading": {
+  //       return {
+  //         ...state,
+  //         isLoading: action.payload
+  //       }
+  //     }
+  //     case "movie/setMovieDetails": {
+  //       return {
+  //         ...state,
+  //         movie: action.payload
+  //       }
+  //     }
+  //     case "movie/setError": {
+  //       return {
+  //         ...state,
+  //         error: action.payload
+  //       }
+  //     }
+  //     default: return state;
+  //   }
+  // };
 
-  export const setIsLoading = (payload) => {
-    return {
-      type: 'movie/setIsLoading',
-      payload,
-    };
-  };
+  // export const setIsLoading = (payload) => {
+  //   return {
+  //     type: 'movie/setIsLoading',
+  //     payload,
+  //   };
+  // };
 
-  export const setMovieDetails = (payload) => {
-    return {
-      type: 'movie/setMovieDetails',
-      payload,
-    };
-  };
+  // export const setMovieDetails = (payload) => {
+  //   return {
+  //     type: 'movie/setMovieDetails',
+  //     payload,
+  //   };
+  // };
 
-  export const setError = (payload) => {
-    return {
-      type: 'movie/setError',
-      payload,
-    };
-  };
+  // export const setError = (payload) => {
+  //   return {
+  //     type: 'movie/setError',
+  //     payload,
+  //   };
+  // };
 
   //{type: "movie/setIsLoading", payload: true | false}
   //{type: "movie/setMovieDetails", payload: {...}}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,21 @@
-import { combineReducers, createStore } from 'redux';
-import { postDetailReducer } from './movieDetailsReducer';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { combineReducers, createStore } from 'redux';
+// import { postDetailReducer } from './movieDetailsReducer';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
 
 
 
-const rootReducer = combineReducers({
-  movie: postDetailReducer,
-});
+// const rootReducer = combineReducers({
+//   movie: postDetailReducer,
+// });
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+import { configureStore } from "@reduxjs/toolkit";
+import { postDetailReducer } from "./movieDetailsReducer";
+
+export const store = configureStore({
+  reducer: {
+    movie: postDetailReducer,
+  }
+})
